dbd687f8bd5fbfbc9cacb6e9f8e59d6843f78ef8
mlbtvps <119069067+mlbtvps@users.noreply.github.com>
2023-05-03 01:38:29 +0630

    Test

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -153,9 +153,9 @@ def multi_rclone_init():
         LOGGER.info("rclone.conf backuped to rclone_bak.conf!")
 
 
-multi_rclone_init()
-app = Client("CendrawasihLeech", bot_token=TG_BOT_TOKEN,
-             api_id=APP_ID, api_hash=API_HASH, workers=343)
-updater = tg.Updater(bot_token=TG_BOT_TOKEN)
-bot = updater.bot
-dispatcher = updater.dispatcher
+#multi_rclone_init()
+#app = Client("CendrawasihLeech", bot_token=TG_BOT_TOKEN,
+             #api_id=APP_ID, api_hash=API_HASH, workers=343)
+#updater = tg.Updater(bot_token=TG_BOT_TOKEN)
+#bot = updater.bot
+#dispatcher = updater.dispatcher
